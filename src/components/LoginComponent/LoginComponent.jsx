ca384bdfa5f707c6798e455ac414db93578acb2b
Balaramansethu <baalaaraman@gmail.com>
2024-02-05 08:11:04 +0530

    Basic login done without UI

--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginComponent = () => {
-  return (
-    <div>LoginComponent</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginComponent
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+
+
+  const formsubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <React.Fragment>
+        <h1>Login</h1>
+
+        <form onSubmit={formsubmitHandler}>
+
+            <div className="details-container">
+
+                <label>EmailID</label>
+                <input
+                    type="text"
+                    className="basic-details"
+                    placeholder="Enter your email address"
+                    value={email}
+                    onChange={emailHandler}
+                    required
+                />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="basic-details"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={passwordHandler}
+                    required
+                />
+
+            
+        
+
+
+                <div className="submit">
+                    <button type="submit" className="btn">Submit</button>
+                </div>
+
+            </div>
+        </form>
+    </React.Fragment>
+  )
+};
+
+export default LoginComponent;
