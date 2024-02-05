d550aefd6d94c7cabc097fc034cf8679e328ad3b
Balaramansethu <baalaaraman@gmail.com>
2024-02-05 06:55:57 +0530

    Initial commit done for file structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginComponent from "./components/LoginComponent/LoginComponent";
+import SignupComponent from "./components/SignupComponent/SignupComponent";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <LoginComponent />
+      <SignupComponent />
+    </React.Fragment>
+  );
+};
+
+export default App;
