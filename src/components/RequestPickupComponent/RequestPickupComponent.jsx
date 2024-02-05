1a3d63428c42e172ca23efa4daccea9f6321c481
Srujana0303 <srujanasanagaram@gmail.com>
2024-02-06 01:58:38 +0530

    ADD: Added PickupRequestComponent

--- src/components/RequestPickupComponent/RequestPickupComponent.jsx
+++ src/components/RequestPickupComponent/RequestPickupComponent.jsx
@@ -1,32 +1,56 @@
-import React from "react";
-import "java.css";
+import React,{useState, useRef} from "react";
+import "../../assets/css/RequestPickupComponent.css";
 
 
 export const RequestPickupComponent = () =>{
+    const form_data = useRef();
+
+    const [form_info,setForm_info] = useState({});
+
+    const form_content = (event) =>{
+        event.preventDefault();
+        const name = form_data.current.waste_name.value;
+        const door_no = form_data.current.door_no.value;
+        const floor = form_data.current.floor.value;
+        const street_name = form_data.current.street_name.value;
+        const postal_code = form_data.current.postal_code.value;
+        const city = form_data.current.city.value;
+        const state = form_data.current.state.value;
+        const country = form_data.current.country.value;
+        setForm_info({name,door_no,floor,street_name,postal_code,city,state,country});
+        event.target.reset();
+    }
+    console.log(form_info);
+    
+
     return(
         <div className="container">
             <br />
+            <h1>Pickup Request form</h1>
             <span>
-                Box
+                <h1>#Add attachment component</h1>
             </span>
-            <span>
-                <input type="text" placeholder="E-Waste Name"/>
+            <form ref={form_data} onSubmit={(events) => form_content(events)}>
+                <input type="text" name="waste_name" placeholder="E-Waste Name"/>
                 <h1>Address</h1>
-                <input type="text" placeholder="Door No"/>
-                <input type="text" placeholder="Floor"/>
-                <input type="text" placeholder="Street Name"/>
-                <input type="text" placeholder="Postal Code" />
-                <input type="text" placeholder="City"/>
-                <input type="text" placeholder="State"/>
-                <input type="text" placeholder="Country"/>
+                <input type="text" name="door_no"placeholder="Door No"/>
+                <input type="text" name="floor" placeholder="Floor"/>
+                <input type="text" name="street_name" placeholder="Street Name"/>
+                <input type="text" name="postal_code" placeholder="Postal Code" />
+                <input type="text" name="city" placeholder="City"/>
+                <input type="text" name="state" placeholder="State"/>
+                <input type="text" name="country" placeholder="Country"/>
+
 
                 <div>
-                    Tags
+                    <h1>#Tags Component</h1>
                 </div>
                 <div>
-                    Description Box
+                   <h1> #Description Box Component</h1>
                 </div>
-            </span>
+                <button type="submit">Submit Form</button>
+
+            </form>
         </div>
     )
 }
